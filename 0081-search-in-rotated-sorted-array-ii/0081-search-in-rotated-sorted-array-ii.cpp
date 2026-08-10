8e8b7e8461486d3e1dc1de1884f53ff041d42285
Sanjai s <sanjaikce@gmail.com>
2026-08-10 15:42:27 +0530

    0081-SEARCH-IN-ROTATED-SORTED-ARRAY-II Stats: Time: 0 ms (100%), Space: 18 MB (82.13%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       int low = 0 , high = nums.size()-1;
+       while(low <= high){
+        int mid = low + ((high - low) >> 1);
+        if(nums[mid] == target){
+            return true;
+        }
+        if(nums[low] == nums[high]){
+            if(nums[low] == target)return mid;
+            low++;high--;
+        }
+        else if(nums[low] <= nums[mid]){
+            if(target >= nums[low] && nums[mid]-1 >= target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        else{
+            if(target >= nums[mid+1] && target <= nums[high]){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+       }
+       return false; 
+    }
+};
--- README.md
+++ README.md
@@ -58,8 +58,10 @@ A collection of LeetCode questions to ace the coding interview! - Created using 
 |  |
 | ------- |
 | [0033-search-in-rotated-sorted-array](https://github.com/sanjais-png/Daily-leetcode/tree/master/0033-search-in-rotated-sorted-array) |
+| [0081-search-in-rotated-sorted-array-ii](https://github.com/sanjais-png/Daily-leetcode/tree/master/0081-search-in-rotated-sorted-array-ii) |
 ## Binary Search
 |  |
 | ------- |
 | [0033-search-in-rotated-sorted-array](https://github.com/sanjais-png/Daily-leetcode/tree/master/0033-search-in-rotated-sorted-array) |
+| [0081-search-in-rotated-sorted-array-ii](https://github.com/sanjais-png/Daily-leetcode/tree/master/0081-search-in-rotated-sorted-array-ii) |
 <!---LeetCode Topics End-->
