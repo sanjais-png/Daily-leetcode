9dacc6a5973612fa19403b216b86f563446d822e
Sanjai s <sanjaikce@gmail.com>
2026-09-03 15:33:29 +0530

    0104-MAXIMUM-DEPTH-OF-BINARY-TREE Stats: Time: 0 ms (100%), Space: 22.3 MB (27.82%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -15,8 +15,8 @@ public:
         if(root == nullptr){
             return 0;
         }
-        int left = 1+maxDepth(root->left);
-        int right = 1+maxDepth(root->right);
-        return max(left , right);
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return 1+max(left , right);
     }
 };
