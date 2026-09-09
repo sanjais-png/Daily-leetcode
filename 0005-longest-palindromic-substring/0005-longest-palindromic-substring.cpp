cb6e38d8dba2d259584f8a8b6144618ac8c39c24
Sanjai s <sanjaikce@gmail.com>
2026-09-09 14:13:25 +0530

    0005-LONGEST-PALINDROMIC-SUBSTRING Stats: Time: 318 ms (15.29%), Space: 293 MB (5.2%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        
-        string ans="";
-        if(s==string(s.rbegin(),s.rend()))return s;
-        for(int i=0;i<s.size();i++){
-            for(int j=s.size()-1;j>=i;j--)
-            {
-                string sub="";
-                if(s[i]==s[j]){
-                    sub=s.substr(i,j-i+1);
-                    string a=string(sub.rbegin(),sub.rend());
-                    if(sub==a)
-                    {
-                        if(ans.size()<sub.size())
-                        ans=sub;
+
+    string lcs(string s , string t){
+        int n = s.size() , m = t.size();
+        if(n == 0 || m == 0){
+            return "";
+        }
+        vector<vector<int>>dp(n+1 , vector<int>(m+1,0));
+        int end = 0 , maxLen = 0;
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                    if(n-j == i-dp[i][j] && maxLen < dp[i][j]){
+                        maxLen = dp[i][j];
+                        end = i;
                     }
+                }else{
+                    dp[i][j] = 0;
                 }
             }
         }
-        if(ans.size()==0)ans=s[0];
-        return ans;
+        int start = end - maxLen;
+        return s.substr(start,maxLen);
+    }
 
+    string longestPalindrome(string s) {
+        string rev = string(s.rbegin() , s.rend());
+        return lcs(s,rev);
     }
 };
