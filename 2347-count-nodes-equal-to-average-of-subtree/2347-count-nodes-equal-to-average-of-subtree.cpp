f13f46c7479cb6997e5ccb72e06b2252a5113213
Sanjai s <sanjaikce@gmail.com>
2026-09-10 10:10:13 +0530

    2347-COUNT-NODES-EQUAL-TO-AVERAGE-OF-SUBTREE Stats: Time: 17 ms (17.15%), Space: 15.7 MB (84.8%) - LeetHub

--- 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.cpp
@@ -12,26 +12,23 @@
 class Solution {
 public:
     int count = 0 , ans = 0 , sum = 0;
-    int traversal(TreeNode* root , int &sum , int &count){
+    int traversal(TreeNode* root){
         if(root == nullptr){
             return 0;
         }
+        traversal(root->left);
         sum += root->val; count += 1;
-        traversal(root->left , sum , count);
-        traversal(root->right , sum , count);
+        traversal(root->right);
         return sum;
     }
     int averageOfSubtree(TreeNode* root) {
         if(root == nullptr){return 0;}
         count = 0 , sum = 0;
-        traversal(root,sum,count);
-
-        cout<<sum<<" "<<count<<endl;
+        traversal(root);
 
         if(sum / count == root->val){
             ans++;
         }
-
 
         averageOfSubtree(root->left);
         averageOfSubtree(root->right);
