d59bf62f63ffba8967977cdfb09af34d872feb5f
Sanjai s <sanjaikce@gmail.com>
2026-09-10 15:21:11 +0530

    0078-SUBSETS Stats: Time: 1 ms (42.26%), Space: 9.9 MB (59.52%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    void rec(vector<int>& nums ,vector<int>curr, int n , int index){
-        if(index >= n){
+    void rec(vector<int>& nums ,vector<int>&curr, int n , int index){
+        if(index == n){
+            ans.push_back(curr);
             return;
         }
-        vector<int>take = curr;
-        take.push_back(nums[index]);
-        ans.push_back(take);
-        rec(nums , take , n , index+1);
+        curr.push_back(nums[index]);
+        rec(nums , curr , n , index+1);
+        curr.pop_back();
         rec(nums , curr , n , index+1);
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
-        ans.push_back({});
-        rec(nums , {} , n , 0);
+        vector<int>curr;
+        rec(nums , curr , n , 0);
         return ans;
     }
 };
